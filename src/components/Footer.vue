18450399a5505c15302a5f2b0a95be4e77cad57d
FrancescoAndreotti <francescoandreotti00@gmail.com>
2021-11-12 17:07:33 +0100

    Added main

---
+++ src/components/Footer.vue
@@ -0,0 +1,117 @@
+<template>
+  <footer>
+    <div class="shop-container">
+      <div class="container">
+        <div class="row">
+          <ul>
+            <li>
+              <img
+                src="../assets/img/buy-comics-digital-comics.png"
+                alt="1"
+              /><a href="#">DIGITAL COMICS</a>
+            </li>
+            <li>
+              <img src="../assets/img/buy-comics-merchandise.png" alt="2" /><a
+                href="#"
+                >DC MERCHANDISE</a
+              >
+            </li>
+            <li>
+              <img src="../assets/img/buy-comics-subscriptions.png" alt="3" /><a
+                href="#"
+                >SUBSCRIPTION</a
+              >
+            </li>
+            <li>
+              <img src="../assets/img/buy-comics-shop-locator.png" alt="4" /><a
+                href="#"
+                >COMIC SHOP LOCATOR</a
+              >
+            </li>
+            <li>
+              <img src="../assets/img/buy-dc-power-visa.svg" alt="5" /><a
+                href="#"
+                >DC POWER VISA</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- DC container -->
+    <div class="dc-container">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div>
+              <h2>DC COMICS</h2>
+              <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+              <h2>SHOP</h2>
+              <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
+            <div>
+              <h2>DC</h2>
+              <ul>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy Policy (new)</a></li>
+                <li><a href="#">Ad Choise</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+            <div>
+              <h2>SITES</h2>
+              <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazines</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-6">
+            <img src="@/assets/img/dc-logo-bg.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Fine DC Container -->
+    <div class="bottom-footer-section">
+      <div class="container">
+        <div class="row">
+          <div>
+            <button type="button">Sign-up now</button>
+          </div>
+          <div>
+            <span>follow us</span>
+            <img src="@/assets/img/footer-facebook.png" alt="" />
+            <img src="@/assets/img/footer-twitter.png" alt="" />
+            <img src="@/assets/img/footer-youtube.png" alt="" />
+            <img src="@/assets/img/footer-pinterest.png" alt="" />
+            <img src="@/assets/img/footer-periscope.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <!-- Logo -->
+        <div class="col-4 logo">
+          <img src="../assets/img/dc-logo.png" alt="logo" />
+        </div>
+        <!-- link -->
+        <div class="col-8">
+          <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+
+
+
+
+
+<style scoped lang="scss">
+</style>
